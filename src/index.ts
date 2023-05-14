e449cd38ef9b967b3880980dd4362f784403efed
mdsams <tabrejmdsams786@gmail.com>
2023-05-14 22:15:00 +0530

    fixing server issue

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,38 @@
-require('dotenv').config()
+require('dotenv').config();
 import mongoose, { connect } from 'mongoose';
 import express, { Express } from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import morgan from 'morgan';
 import { PORT } from './config/server-config';
 import logger from './utils/logger';
 import OTPRoutes from './routes/otp-routes';
+import passport from 'passport';
+import { UserOtpStrategy } from './strategies/otp';
 
-
-mongoose.set("strictQuery", false);
-connect(process.env.MONGO_URI || "mongo", (err) => {
-  if (err)
-    throw Error("Cannot connected to db");
-  else
-    logger.info("Successfully connected to db");
-})
+mongoose.set('strictQuery', false);
+connect(process.env.MONGO_URI || 'mongo', (err) => {
+	if (err) throw Error('Cannot connected to db');
+	else logger.info('Successfully connected to db');
+});
 
 // Create an express server
 const app: Express = express();
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors())
+app.use(cors());
 
+passport.use('userOtp', UserOtpStrategy);
 app.use('/auth/userOtp/', OTPRoutes);
 
 // app.listen(PORT, () => {
 //   logger.info(`Application listening on http://localhost:${PORT}`);
 // })
 
-
 try {
-  app.listen(PORT, () => {
-    logger.info(`Application listening on http://localhost:${PORT}`);
-  });
+	app.listen(PORT, () => {
+		logger.info(`Application listening on http://localhost:${PORT}`);
+	});
 } catch (error) {
-  logger.error('An error occured while starting the server');
-  throw new Error('An error occured while starting the server');
+	logger.error('An error occured while starting the server');
+	throw new Error('An error occured while starting the server');
 }
---
+++ src/strategies/otp.ts
@@ -0,0 +1,66 @@
+import { Strategy } from 'passport-local';
+import otpModel from '../models/otp';
+import userModel from '../models/users';
+import { sign } from 'jsonwebtoken';
+import logger from '../utils/logger';
+import { ErrorCode, HandyManError } from '../utils/error';
+
+const UserOtpStrategy = new Strategy(async function verify(mobile, otp, cb) {
+	console.log('user verified');
+	try {
+		// Get the otp from OTPs collection
+		const OTPdata = await otpModel.findOne({ mobileNumber: mobile });
+
+		// Throw error if you cant find the entry in OTPs collections
+		// Either the TTL has expired or the client sent wrong details
+		if (OTPdata === null) {
+			throw new HandyManError(ErrorCode.BAD_DATA, `Invalid mobile number ${mobile}`);
+		} else if (OTPdata.OTPValue !== otp) {
+			throw new Error('Wrong OTP.');
+		}
+
+		// Find if the user already exists
+		const foundUser = await userModel
+			.findOne(
+				{
+					providers: {
+						$elemMatch: {
+							provider: 'MOBILE',
+							uid: mobile,
+						},
+					},
+				},
+				{
+					providers: 0,
+				},
+			)
+			.lean();
+
+		// If the user already exists, issue a token to user
+		if (foundUser !== null) {
+			const jwt = sign(foundUser, 'SuperSecretKey', { algorithm: 'HS256', expiresIn: '4d' });
+			return cb(null, { user: foundUser, token: jwt });
+		}
+
+		//If the user doesn't exist, create a new user
+		const newUser = new userModel({
+			providers: [
+				{
+					name: 'MOBILE',
+					uid: mobile,
+				},
+			],
+		});
+
+		const savedUser = await newUser.save();
+		if (savedUser !== null) {
+			const jwt = sign(savedUser.toObject(), 'SuperSecretKey', { algorithm: 'HS256', expiresIn: '4d' });
+			return cb(null, { user: savedUser.toObject(), token: jwt });
+		}
+	} catch (error: any) {
+		logger.error(error.stack);
+		cb(error, undefined);
+	}
+});
+
+export { UserOtpStrategy };
